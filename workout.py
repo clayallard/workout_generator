ad3dfc4c9b286ae1ee137ecafa1757486d149530
Clayton Allard <91763921+clayallard@users.noreply.github.com>
2024-07-11 20:01:08 -0700

    Add files via upload

--- workout.py
+++ workout.py
@@ -17,12 +17,14 @@ from urllib.request import urlopen
 class Workout:
 
     # day will be today if it is the morning and tomorrow if it is the next day
-    def __init__(self, work_type, specifics,
+    def __init__(self, work_type, specifics, pre_work,
                  day=date.today() if datetime.now().strftime("%H") < "12" else date.today() + timedelta(1)):
         self.details = work_type
         self.specs = specifics
         self.day = day
         self.workout = ""
+        self.pre_work = pre_work
+        self.daily_work = None
 
     def __run_case__(self, details=[]):
         if "run" in details:
@@ -31,17 +33,15 @@ class Workout:
                                     "Intermediate distance sprints", "Short distance sprints"],
                                    p=[.01, .01, .03, .75, .1, .1])
             # endurance = np.random.choice(["Endurance", "Intermediate", "Short spurts"], p=[.75, .15, .1])
-            run += " or " + np.random.choice(
-                ["Treadmill", "Bike", "Stairmaster", "Row", "Ski machine", "Jacob's Ladder"],
-                p=[.35, .3, .1, .08, .07, .1]) + " if weather is unsuitable\n"
+            run += " or " + self.__machine__([.3, .3, .15, .15, .1])+"\n"
             return run
         return self.__machine__() + "\n"
 
-    def __machine__(self):
+    def __machine__(self, p=[.2, .3, .25, .15, .1]):
         endurance = np.random.choice(["Long long endurance", "long endurance", "Endurance", "Intermediate", "Short spurts"], p=[.01, .02, .57, .2, .2])
         return endurance + " " + np.random.choice(
-            ["Treadmill", "Bike", "Stairmaster", "Row", "Ski machine", "Jacob's Ladder"],
-            p=[.2, .3, .1, .15, .1, .15])
+            ["Treadmill", "Bike", "Stairmaster", "Row", "Ski machine"],
+            p=p)
 
     def __reps__(self, lift):
         if "Dumbbell" in lift:
@@ -51,10 +51,13 @@ class Workout:
     def __gym_case__(self, details):
         routine = ""
         ab = False
-        if np.random.random() < .25:
+        extra = 0.175
+        if np.random.random() < extra:
             routine += "Hold plank as long as possible\n"
-        if np.random.random() < .25:
+        if np.random.random() < extra:
             routine += "Hang pull ups\n"
+        if np.random.random() < extra:
+            routine += "Toes to bar\n"
         if "abs" in details:
             routine += "Ab workout\n"
         if "cf" in details:
@@ -66,7 +69,7 @@ class Workout:
             if routine != "":
                 optional = " (if time persists)"
                 func = np.random.choice([self.__weights__, self.__cf__, self.__machine__, self.__run_case__],
-                                        p=np.array([.7, .1, .1, .1]))
+                                        p=np.array([.64, .12, .12, .12]))
             return routine + func() + optional + "\n"
         return routine
 
@@ -82,22 +85,22 @@ class Workout:
         return l1 + l2
 
     def __cf__(self):
-        if np.random.random() < .5:
-            return "Quinn's workout"
+        # if np.random.random() < .5:
+        #     return "Quinn's workout"
         dow = self.day.weekday()
         cf_days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
         rand_day = np.random.randint(0, 7)
         cf_day = self.day - timedelta(days=(dow - rand_day) % 7)
         return cf_days_of_week[rand_day] + " " + str(cf_day) + " Crossfit Workout"
 
-        cf_days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-        ref_date = date(2023, 5, 28)
-        # 0 % 4 is a rest day
-        day_in_cycle = ((self.day - ref_date).days + 1) % 4
-        choice = np.random.choice([i for i in range(4) if i != day_in_cycle])
-        cf_day = self.day - timedelta(int(choice))
-        dow = cf_day.weekday()
-        return f"{cf_days_of_week[dow]} {cf_day} Crossfit Workout"
+        # cf_days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+        # ref_date = date(2023, 5, 28)
+        # # 0 % 4 is a rest day
+        # day_in_cycle = ((self.day - ref_date).days + 1) % 4
+        # choice = np.random.choice([i for i in range(4) if i != day_in_cycle])
+        # cf_day = self.day - timedelta(int(choice))
+        # dow = cf_day.weekday()
+        # return f"{cf_days_of_week[dow]} {cf_day} Crossfit Workout"
 
     def __warm_up__(self):
         return "Coming soon"
@@ -105,8 +108,10 @@ class Workout:
     def __workout_type_configurator__(self):
         specifics = self.specs
         self.workout = "Workout for " + str(self.day) + ":\n\n"
-        morning_night = np.random.choice(["50 Push ups", "50 Sit ups", "50 Squats", "Break"], size=2)
-        self.workout += morning_night[0] + "\n\n"
+        self.daily_work = np.random.choice(["push ups", "sit ups", "squats", "break"], size=2)
+        if self.daily_work[0] != 'break':
+            work = self.daily_work[0]
+            self.workout += f'{self.pre_work[work]["num"]} {work}\n\n'
         if self.details == "run":
             self.workout += self.__run_case__(specifics)
         elif self.details == "gym":
@@ -119,42 +124,14 @@ class Workout:
             self.workout += self.__gym_case__(specifics) + "\n" + self.__run_case__(specifics)
         else:
             raise Exception("Invalid workout type")
-        self.workout += "\n" + morning_night[1]
+        if self.daily_work[1] != 'break':
+            work = self.daily_work[1]
+            self.workout += f'\n{self.pre_work[work]["num"]} {work}'
 
     def workout_of_day(self):
         if self.workout == "":
             self.__workout_type_configurator__()
         return self.workout
-
-
-# def init_json():
-#     init_file = {"structure": {"2023-2": {0: {"type": "gym/run", "details": ["cf", "run"]},
-#                                           1: {"type": "off", "details": []},
-#                                           2: {"type": "gym", "details": ["plank", "weights"]},
-#                                           3: {"type": "run", "details": ["run"]},
-#                                           4: {"type": "off", "details": []},
-#                                           5: {"type": "gym", "details": ["weights"]},
-#                                           6: {"type": "run", "details": ["run"]},
-#                                           7: {"type": "run/gym", "details": ["machine", "abs", "weights"]},
-#                                           8: {"type": "off", "details": []},
-#                                           9: {"type": "gym", "details": ["weights"]},
-#                                           10: {"type": "run", "details": ["run"]},
-#                                           11: {"type": "off", "details": []},
-#                                           12: {"type": "gym", "details": ["cf"]},
-#                                           13: {"type": "run", "details": ["run"]},
-#                                           }},
-#                  "last_save": str(date.today())}
-#     with open("details.json", "w") as f:
-#         json.dump(init_file, f)
-#
-#     first_workout = "Workout for 2023-07-30:\n\nBike\n\nAb workout\nDumbbell Bench for max set of 10 reps\nDeadlift " \
-#                     "for max set of 8 reps (if time persists) "
-#
-#     # workout data
-#     df = pd.DataFrame({"Date": [str(date.today())], "Type": ["run/gym"], "Details": [["abs", "weights", "machine"]],
-#                        "Workout": [first_workout]})
-#     df.set_index("Date", inplace=True)
-#     df.to_csv("workout_history.csv")
 
 
 def workout_structure_generator():
@@ -223,13 +200,13 @@ def workout_structure_generator():
     # plank_day = gym_days[4]
     # abs_day = gym_days[5]
     run_days = [str(r) for r in range(len(workout_types)) if "run" in workout_types[r]]
-    np.random.shuffle(gym_days)
+    np.random.shuffle(run_days)
     details = {}
     for i in range(len(workout_types)):
         details[str(i)] = {"type": workout_types[i], "details": []}
     # inputting the specific kind of workouts
     details[gym_days[4]]["details"] += ["abs"]
-    # details[gym_days[5]]["details"] += ["abs"]
+    details[gym_days[5]]["details"] += ["abs"]
     details[run_days[0]]["details"] += ["machine"]
     for i in range(2):
         details[gym_days[i]]["details"] += ["cf"]
@@ -244,25 +221,35 @@ def __get_version__(day):
     return day.strftime("%Y") + "-" + str(int((int(day.strftime("%m")) - 1) / 4) + 1)
 
 
-def create_workout(day, details, df):
+def create_workout(day, details, df, pre_workout):
     ref_date = date(year=2023, month=5, day=28)
     cycle = (day - ref_date).days % 14
     if day.strftime("%m%d") in ["0101", "0501", "0901"]:
         details["structure"][__get_version__(day)] = workout_structure_generator()
     typ = details["structure"][__get_version__(day)][str(cycle)]["type"]
     dets = details["structure"][__get_version__(day)][str(cycle)]["details"]
-    w = Workout(typ, dets, day)
+    w = Workout(typ, dets, pre_workout, day)
     w.workout_of_day()
     date_format = day.strftime("%Y-%m-%d")
-    df.loc[date_format] = [typ, dets, w.workout]
+    df.loc[date_format] = [typ, dets, w.workout, w.daily_work]
+    # inefficient but gets the job done
+    pre_workout["unsaved"] = [x for x in pre_workout["unsaved"] if x[0] != date_format]
+    # add the daily workouts
+    for w in w.daily_work:
+        if w == 'break':
+            continue
+        pre_workout["unsaved"] += [[date_format, w, 0]]
+    pre_workout["unsaved"].sort(key=lambda x: x[0])
 
 
-def output(details, df):
+def output(details, df, pre_workout):
     details["last_save"] = str(max(df.index))
     df.sort_index(ascending=False, inplace=True)
     df.to_csv("workout_history.csv")
     with open("details.json", "w") as f:
         json.dump(details, f)
+    with open("pre_workout.json", "w") as f:
+        json.dump(pre_workout, f)
 
 
 def workout_structure(details, day):
@@ -278,6 +265,72 @@ def workout_structure(details, day):
         detas = struc[str(s)]["details"]
         event = ", ".join(detas)
         print(f"{dow} {cycle}: {typ} -> {event}")
+
+
+def handle_preworkouts(pre_workout):
+    # instructions to user
+    pre_date = lambda da: da[0] > str(datetime.today())
+    workout_queue = [p for p in pre_workout["unsaved"] if pre_date]
+    size = len(workout_queue)
+
+    print(f"There are {size} workouts to review")
+    print(f"y - completed\nya - completed all\ni - completed but not in a row" +
+          f"\nn - not completed\nna - all not completed\ns - skip\nsa - skip all\nk - next without skipping\nq - quit")
+
+    # if workout is completed
+    def complete(w, eq = 1):
+        # eq should be 1 or -1
+        val = pre_workout[w]['str']
+        if val == eq:
+            pre_workout[w]['num'] += eq
+            pre_workout[w]['str'] = 0
+        else:
+            pre_workout[w]['str'] = eq
+
+    def not_done(w):
+        pre_workout[w]['str'] = 0
+        pre_workout[w]['num'] -= 1
+
+    # if ya or na is selected
+    do_all = False
+
+    for i, daily_work in enumerate(workout_queue):
+        d, w, _ = daily_work
+        if not do_all:
+            response = input(f'Did you complete {pre_workout[w]["num"]} {w} on {d}?')
+        # make no changes if q
+        if response == 'q':
+            break
+        if response == 'k':
+            continue
+        if response == 'sa':
+            do_all = True
+            response = 's'
+        # ensure everything is y
+        if response == 'ya':
+            do_all = True
+            response = 'y'
+        # ensure everything is n
+        if response == 'na':
+            do_all = True
+            response = 'n'
+        if response == 's':
+            pre_workout['unsaved'][i][2] = 1
+        elif response == 'y':
+            complete(w)
+            pre_workout['unsaved'][i][2] = 1
+        elif response == 'i':
+            complete(w, -1)
+            pre_workout['unsaved'][i][2] = 1
+        elif response == 'n':
+            not_done(w)
+            pre_workout['unsaved'][i][2] = 1
+
+    pre_workout['unsaved'] = [daily_work for i, daily_work in enumerate(workout_queue) if daily_work[2] == 0]
+
+    with open("pre_workout.json", "w") as f:
+        json.dump(pre_workout, f)
+
 
 
 def main():
@@ -301,6 +354,9 @@ def main():
                         help="Get tomorrow's workout. This has priority over the date argument", action="store_true")
     parser.add_argument("-s", "--structure", default=False,
                         help="Get workout structure for specified date", action="store_true")
+    parser.add_argument("-p", "--pre", default=False,
+                        help="input the result of the daily push ups, sit ups, squats workouts", action="store_true")
+
     args = parser.parse_args()
     day = date.today() + timedelta(1) if args.tomorrow and not args.today else args.date
     day = date.today() if args.today else day
@@ -312,6 +368,12 @@ def main():
     # import data
     with open("details.json", "r") as f:
         details = json.load(f)
+    with open("pre_workout.json", "r") as f:
+        pre_workout = json.load(f)
+
+    # This takes precedents
+    if args.pre:
+        handle_preworkouts(pre_workout)
     df = pd.read_csv("workout_history.csv", index_col="Date")
 
     start = datetime.strptime(details["last_save"], "%Y-%m-%d").date()
@@ -323,7 +385,9 @@ def main():
             print("No version exists for this date")
         return
     if clear:
+        # delete entries
         df = df.loc[str(day) >= df.index]
+        # delete versions
         recent_ver = __get_version__(datetime.strptime(max(df.index), "%Y-%m-%d").date())
         sorted_versions = sorted(details["structure"].keys())
         ind_of = sorted_versions.index(recent_ver)
@@ -331,6 +395,10 @@ def main():
         for d in delete:
             details["structure"].pop(d)
             print("Version " + d + " has been removed.")
+        # delete pre-workouts
+        pre_date = lambda da: da[0] >= str(day)
+        idx_remove = pre_workout["unsaved"].index(next(filter(pre_date, pre_workout["unsaved"])))
+        pre_workout["unsaved"] = pre_workout["unsaved"][:idx_remove]
         print("All workouts beyond the day " + str(day) + " have been removed")
     elif day <= start:
         if not reset:
@@ -338,22 +406,22 @@ def main():
                 print(df.loc[str(day)]["Workout"])
             else:
                 print("No recorded workout for this day")
-            output(details, df)
+            output(details, df, pre_workout)
             return
         elif reset_all:
             day_count = (start - day).days + 1
             for single_date in (day + timedelta(n) for n in range(day_count)):
-                create_workout(single_date, details, df)
+                create_workout(single_date, details, df, pre_workout)
         else:
-            create_workout(day, details, df)
+            create_workout(day, details, df, pre_workout)
     else:
         day_count = (day - start).days
         for single_date in (start + timedelta(n + 1) for n in range(day_count)):
-            create_workout(single_date, details, df)
+            create_workout(single_date, details, df, pre_workout)
 
     print(df.loc[str(day)]["Workout"])
 
-    output(details, df)
+    output(details, df, pre_workout)
 
     # w = Workout("run/gym", ["abs", "weights", "machine"])
     # print(w.workout_of_day())
